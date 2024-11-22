905a94a2b08e84741f54b50e758c1461f65264d5
Tiffany Cordova <tiffany.cordova@proton.me>
2024-11-22 09:28:37 -0800

    Update add movie to favorites

--- index.js
+++ index.js
@@ -63,17 +63,18 @@ app.put('/users/:Username', async (req, res) => {
   });
 
 // CREATE Allow users to add a movie to their list of favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-  
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    } else {
-        res.status(400).send('no such user');
-    }
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+       $push: { FavoriteMovies: req.params.MovieID }
+     },
+     { new: true }) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(‘Error: ’ + err);
+    });
   });
 
 // READ
